562b19ccc70e50b31e49106e028b576d96d4cfcb
Bunro <brun95@gmx.de>
2019-12-12 02:11:00 +0100

    fridge view update whole

--- lib/screens/fridge.dart
+++ lib/screens/fridge.dart
@@ -1,7 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:forager/model/item.dart';
+import 'package:forager/widgets/fridge/fridgeListTile.dart';
 import 'shopping_list.dart';
+import 'package:rflutter_alert/rflutter_alert.dart' as prefix0;
+import 'package:intl/intl.dart';
+import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
 
-class FridgePage extends StatelessWidget {
+
+class FridgePage extends StatefulWidget {
+  @override
+  _FridgePageState createState() => _FridgePageState();
+
+}
+
+class _FridgePageState extends State<FridgePage> {
+  static DateTime time= new DateTime.now();
+  List<Item> items=[(new Item(id: "5045",name: "milk",expiryDate: time.add((new Duration(days: 60))))),(new Item(id: "5555",name: "eggs",expiryDate: time.add((new Duration(days: 30))))),(new Item(id: "01",name: "water",expiryDate: time.add((new Duration(days: 15))))) ];
+
+  selectDate(BuildContext context){
+    return prefix0.Alert(
+      context: context,
+      title: "select expiry date",
+      content: Card(
+        child: BasicDateField(),
+      ),
+      buttons: [
+        prefix0.DialogButton(
+         child: Text("set date"),
+    )
+        ]
+    ).show();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,14 +46,53 @@ class FridgePage extends StatelessWidget {
           )
         ],
       ),
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            // Navigate back to first route when tapped.
-          },
-          child: Text('Go back!'),
+      body: Builder(
+          builder: (context) => Center(
+          child: Column(
+            children: <Widget>[
+              FridgeItem(
+                itemsList: items,
+                handleItem: (int index, String action){
+                  setState(() {
+                    if(action=="delete") {
+                      items.removeAt(index);
+                      for (var x in items) {
+                        print(x.name);
+                      }
+                    }else if(action=="move"){
+                      //TODO: add item to shopping list
+                      print("add item ${items[index].name} to shopping list");
+                    } else if(action=="selectDate"){
+                      selectDate(context);
+                    }
+                  });
+
+                },
+              )
+            ],
+          ),
         ),
       ),
     );
+  }
+}
+
+class BasicDateField extends StatelessWidget {
+  final format = DateFormat("yyyy-MM-dd");
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: <Widget>[
+      Text('Basic date field (${format.pattern})'),
+      DateTimeField(
+        format: format,
+        onShowPicker: (context, currentValue) {
+          return showDatePicker(
+              context: context,
+              firstDate: DateTime(1900),
+              initialDate: currentValue ?? DateTime.now(),
+              lastDate: DateTime(2100));
+        },
+      ),
+    ]);
   }
 }
---
+++ lib/widgets/fridge/fridgeListTile.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:forager/model/item.dart';
+
+
+class FridgeItem extends StatelessWidget {
+  final List<Item> itemsList;
+  final Function(int, String) handleItem;
+  FridgeItem({Key key, this.itemsList, this.handleItem}): super(key: key);
+
+  Widget _buildProductItem(BuildContext context, int index) {
+    return Card(
+     child: Dismissible(
+         key: Key(itemsList[index].id),
+        child: InkWell(
+              onTap: (){
+                handleItem(index, "selectDate");
+              },
+              onLongPress: (){
+
+              },
+              child: ListTile(
+                leading: Icon(Icons.fastfood),
+                title: Text(
+                  itemsList[index].name,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                subtitle: Text('expiry date: ${itemsList[index].expiryDate.day}.${itemsList[index].expiryDate.month}.',
+                    textAlign: TextAlign.center,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(fontStyle: FontStyle.italic, fontWeight: FontWeight.bold, fontSize: 20)),
+              ),
+        ),
+        onDismissed: (DismissDirection dir){
+           if(dir==DismissDirection.startToEnd){
+             handleItem(index, "delete");
+           } else handleItem(index, "delete");
+          Scaffold.of(context).showSnackBar(
+              SnackBar(
+                content: Text(dir!= DismissDirection.startToEnd
+                    ? 'moved item to shopping list'
+                    : 'deleted item'),
+              )
+          );
+        },
+       background: Container(
+         color: Colors.green,
+         child: Icon(Icons.remove),
+         alignment: Alignment.centerLeft,
+       ),
+       secondaryBackground: Container(
+         color: Colors.blue,
+         child: Icon(Icons.add_shopping_cart),
+         alignment: Alignment.centerRight,
+       ),
+      ),
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      scrollDirection: Axis.vertical,
+      shrinkWrap: true,
+      itemBuilder: _buildProductItem,
+      itemCount: itemsList.length,
+    );
+  }
+}
