2db89a1ebeb0992ba8699dbeb0917ad5b8fb5c51
hlnstepanova_enveto <hlnstepanova@yandex.ru>
2019-12-12 20:11:47 +0100

    Classes item and shop added

---
+++ lib/model/item.dart
@@ -0,0 +1,8 @@
+class Item {
+  String id;
+  String name;
+  DateTime expiryDate;
+
+  Item ({this.id, this.name, this.expiryDate});
+
+}
---
+++ lib/model/shop.dart
@@ -0,0 +1,10 @@
+class Shop {
+  String id;
+  String name;
+  String address;
+  double longitude;
+  double latitude;
+
+  Shop ({this.id, this.name, this.address, this.longitude, this.latitude });
+
+}
