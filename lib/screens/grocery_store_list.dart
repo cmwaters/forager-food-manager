e37e065a996e99300175c886f3b9ed57244bc03a
Callum Michael Waters <c.waters@campus.tu-berlin.de>
2019-12-12 11:10:19 +0100

    added stores

--- lib/screens/grocery_store_list.dart
+++ lib/screens/grocery_store_list.dart
@@ -2,27 +2,45 @@ import 'package:flutter/material.dart';
 import 'package:forager/screens/supermarket_map.dart';
 
 class StorePage extends StatelessWidget {
+
+  var stores = [['Rewe','Turmstraße 32'], ['Netto','Sonnenallee 45'], ['Lidl', 'Weberstraße 7']];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Find Your Supermarket"),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.arrow_forward),
-            onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) => SupermarketMap()));
-            },
-          )
-        ],
+//        actions: <Widget>[
+//          IconButton(
+//            icon: Icon(Icons.arrow_forward),
+//            onPressed: () {
+//              Navigator.push(context, );
+//            },
+//          )
+//        ],
       ),
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            // Navigate back to first route when tapped.
-          },
-          child: Text('RUnnnnnnnn!'),
-        ),
+      body: ListView.builder(
+        itemCount: 3,
+        itemBuilder: (BuildContext context, int index) {
+          return new InkWell(
+            onTap: () {
+             Navigator.push(context, MaterialPageRoute(builder: (context) => SupermarketMap()));
+            },
+            child: Card(
+              child:ListTile(
+                leading: Icon(Icons.store),
+                title: Text(
+                  stores[index][0],
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                subtitle: Text(
+                  stores[index][1],
+                  style: TextStyle(fontStyle: FontStyle.italic),
+                ),
+              )
+            )
+          );
+        },
       ),
     );
   }
